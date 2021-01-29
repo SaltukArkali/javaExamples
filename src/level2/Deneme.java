5c259a18582244a4427f6d9817bdb4a197958c87
Saltuk <saltukarkali@gmail.com>
2021-01-30 00:56:06 +0100

    local commit saltuk

---
+++ src/level2/Deneme.java
@@ -0,0 +1,34 @@
+package level2;
+
+import java.util.Vector;
+
+public class Deneme {
+
+	public static void main(String[] args) {
+
+
+		deneme();
+		
+		Vector<String> kisilerVektor = new Vector<>();	
+		kisilerVektor.add("ali");
+		kisilerVektor.add("melih");
+		kisilerVektor.add("cengiz");
+		kisilerVektor.add("cavit");
+		kisilerVektor.add("yusuf");
+		System.out.println("vektor listem" + kisilerVektor);
+		
+		
+
+	}
+	public static void deneme() {
+		int a = 1;
+		int b= 2;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+	
+	
+	
+	
+
+}
